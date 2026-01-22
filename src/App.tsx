428410e1aa8ae10574289835b8007b3f79b43ba2
BrewTheFox <157254614+BrewTheFox@users.noreply.github.com>
2026-01-22 16:57:54 -0500

    Refactor project structure and update dependencies; enhance README and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+"use client"
+
 import './App.css'
+import { useState, useEffect } from 'react'
+import { ReactLenis, useLenis } from 'lenis/react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  type Position = 'sticky' | 'relative';
+  const [scrollStatus, setScrollStatus] = useState<Position>("sticky") 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='scrollContainer'>
+      <div className='stickyScroll' style={{position:scrollStatus}}>
+        <h1 className='title'>BrewTheFox</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
