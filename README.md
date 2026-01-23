a8d20d2297c422812b1a50095f78de54209695c7
BrewTheFox <157254614+BrewTheFox@users.noreply.github.com>
2026-01-23 17:00:13 -0500

    feat: add embla-carousel-react for carousel functionality
    - Replace Original Starfield npm with the source code of the git version
    - Developed a `Carousel` component with navigation buttons and context for managing state.
    - Updated App.css for new layout and animations.
    - Refactored App.tsx to integrate new components and manage scroll animations.
    - Updated index.css with new color variables for improved theming.

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-
-
-
 # brewthefox.dev
+
 - Is the domain that **Gemini** made me buy (I was asking it how to smuggle a fursuit to avoid paying taxes).
 - Here I will try to create an amazing introduction website.
 - If I'm not able to renew the domain by the next year all my infrastructure will fall for a day or two.
 - This is probably not going to be my main portfolio.
+
 ---
+
+# Open Source Libraries Used:
+
+- [Starfield](https://medium.com/designly/how-to-create-an-animated-space-stars-background-effect-in-react-next-js-2806b630379c) by designly1 - MIT LICENSE
--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@
 
 import './App.css'
 import { useState, useEffect } from 'react'
-import Starfield from 'react-starfield';
-import {Sparkle} from 'lucide-react';
+import Starfield from './components/starfield';
+import {Sparkle, ArrowDown} from 'lucide-react';
 import { ReactLenis } from 'lenis/react'
 import Fox from './assets/Fox.svg?react'
+import { useMotionValueEvent, useScroll } from "motion/react"
 
 function App() {
-  type Position = 'sticky' | 'relative';
+  type Position = 'sticky' | 'fixed' | 'relative';
+  type Animation = 'infinite' | 0
   const [stickyStatus, setStickyStatus] = useState<Position>("sticky")
+  const [animationIterations, setAnimationIterations] = useState<Animation>("infinite")
   const [quote, setQuote] = useState("I'm a backend developer!")
+  const { scrollYProgress } = useScroll()
+  const [scrollAdviseOpacity, setScrollAdviseOpacity] = useState(1) 
+
   function calcFox(clientX:number, clientY:number, innerHeight:number, innerWidth:number) {
     const x = (clientX / innerWidth - 0.5) * 2;
     const y = (clientY / innerHeight - 0.5) * 2;
@@ -18,11 +24,22 @@ function App() {
     const rotationX = -y * 5; // Max 20 degrees
     const rotationY = x * 5; // Max 20 degrees
     const foxElement = document.getElementById('fox-svg');
-
     if (foxElement) {
           foxElement.style.transform = `perspective(1000px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
     }
   }
+  
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    if (latest == 0) {
+      setAnimationIterations('infinite')
+      setScrollAdviseOpacity(1)
+    }
+    else {
+      setAnimationIterations(0)
+      setScrollAdviseOpacity(0)
+    }
+  })
+
   return (
     <ReactLenis root>
     <div className='scrollContainer'>
@@ -35,8 +52,8 @@ function App() {
           speedFactor={0.05}
           backgroundColor="black"
         />
-        <div style={{display:'flex'}}>
-          <div className='scrollTextContainer'>
+        <div className='subMain'>
+          <div>
             <h1 className='title'>BrewTheFox</h1>
             <div style={{display:'flex'}}>
               <div>
@@ -49,6 +66,13 @@ function App() {
           </div>
           <div>
             <Fox id="fox-svg" className='fox fill-white' style={{ transition: 'transform 0.1s ease-out' }}></Fox>
+          </div>
+        </div>
+        <div>
+          <div style={{display:'flex', justifyContent:'center', justifyItems:'center'}}>
+            <ArrowDown size='3vw' className='arrows' style={{animationIterationCount:animationIterations, opacity:scrollAdviseOpacity}} ></ArrowDown>
+            <span className='scrollAdvise' style={{animationIterationCount:animationIterations, opacity:scrollAdviseOpacity}}>Scroll to know more</span>
+            <ArrowDown size='3vw' className='arrows' style={{animationIterationCount:animationIterations, opacity:scrollAdviseOpacity}}></ArrowDown>
           </div>
         </div>
       </div>
