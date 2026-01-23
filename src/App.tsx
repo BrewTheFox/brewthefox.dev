fe7fa46d73ee1d6187eed7b2773550a2c231fb92
BrewTheFox <157254614+BrewTheFox@users.noreply.github.com>
2026-01-23 12:17:07 -0500

    Add Starfield background and Fox SVG component; enhance layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,40 @@
 import './App.css'
 import { useState, useEffect } from 'react'
 import { ReactLenis, useLenis } from 'lenis/react'
+import Starfield from 'react-starfield';
+import {Sparkle} from 'lucide-react';
+import Fox from './assets/Fox.svg?react'
 
 function App() {
   type Position = 'sticky' | 'relative';
-  const [scrollStatus, setScrollStatus] = useState<Position>("sticky") 
+  const [scrollStatus, setScrollStatus] = useState<Position>("sticky")
+
   return (
     <>
     <div className='scrollContainer'>
       <div className='stickyScroll' style={{position:scrollStatus}}>
-        <h1 className='title'>BrewTheFox</h1>
+        <Starfield
+          starCount={1000}
+          starColor={[255, 255, 255]}
+          speedFactor={0.05}
+          backgroundColor="black"
+        />
+        <div style={{display:'flex'}}>
+          <div className='scrollTextContainer'>
+            <h1 className='title'>BrewTheFox</h1>
+            <div style={{display:'flex'}}>
+              <div>
+                <Sparkle size="2vw" color='white' style={{marginTop:'4vh', marginRight:"1vw"}}></Sparkle>
+              </div>
+              <div>
+                <h2 className='subtitle'>I'm a backend developer!</h2>
+              </div>
+            </div>
+          </div>
+          <div>
+            <Fox className='fox fill-white'></Fox>
+          </div>
+        </div>
       </div>
     </div>
     </>
